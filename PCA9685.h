1a0afe3b1fa608cfd52e14a491c24ff5333202b8
Luuke <lukasz.bartosz1990@gmail.com>
2021-05-07 19:59:01 +0200

    Added files and empty class

---
+++ PCA9685.h
@@ -0,0 +1,30 @@
+/*****************************************************************
+ * Copyright (c) 2021 CodeAbove.
+ * All rights reserved.
+ *
+ * This software component is licensed by CodeAbove under
+ * BSD 3-Clause license, the 'License'. You may not use
+ * this file except in compliance with the License. You
+ * may obtain a copy of the License at:
+ *                  opensource.org/licenses/BSD-3-Clause
+ ****************************************************************/
+#ifndef PCA9685_H
+#define PCA9685_H
+
+namespace CodeAbove::Libraries
+{
+    
+class PCA9685
+{
+public:
+    PCA9685();
+
+
+private:
+
+
+};
+
+} // namespace CodeAbove::Libraries
+
+#endif // PCA9685_H
